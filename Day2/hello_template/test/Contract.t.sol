7e461feb39b26682a2b9a9ce133fb64dc06bbf70
dtmkeng <phumaisut@gmail.com>
2022-08-02 21:51:40 +0700

    add note des playable

--- Day2/hello_template/test/Contract.t.sol
+++ Day2/hello_template/test/Contract.t.sol
@@ -5,11 +5,24 @@ import "forge-std/Test.sol";
 
 import "src/Contract.sol";
 
+
+contract Safe {
+    receive() external payable {}
+
+    function withdraw() external {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
+
 contract TestContract is Test {
+    receive() external payable {}
+
+    Safe safe;
     Contract c;
 
     function setUp() public {
         c = new Contract();
+        safe = new Safe();
     }
 
     function testBar() public {
@@ -24,5 +37,18 @@ contract TestContract is Test {
     function testGetName() public {
         string memory name = c.getName();
         assertEq("keng", name);
+    }
+
+    function testWithdraw() public {
+        // transfer 1 ether to safe
+        payable(address(safe)).transfer(1 ether);
+        // get balance this contract after transfer
+        uint256 preBalance = address(this).balance;
+        // call withdraw function should with draw all ether to this contract
+        safe.withdraw();
+        // get balance after withdraw
+        uint256 postBalance = address(this).balance;
+        // check balance
+        assertEq(preBalance + 1 ether, postBalance);
     }
 }
